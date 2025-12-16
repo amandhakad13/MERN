631c2f9a57205f54b6c0068660e5024198ca9ae8
amandhakad13 <ad5538694@gmail.com>
2025-12-15 18:12:01 -0800

    Day 2

---
+++ Backend/Day 2/first.js
@@ -0,0 +1,13 @@
+// const sum = require("./second");
+// const sub = require("./third");
+// const mul = require("./fourth");
+
+import sum from "./second.js";
+import sub from "./third.js";
+import mul from "./fourth.js";
+
+sum(3,5);
+// sub(8,4);
+// mul(5,7);
+
+console.log("End here");
---
+++ Backend/Day 2/multiple import in single line/first.js
@@ -0,0 +1,11 @@
+// const sum = require("./calculate/sum");
+// const sub = require("./calculate/sub");
+// const mul = require("./calculate/mul");
+
+const {sum, sub, mul} = require("./calculate");
+
+sum(5,4);
+sub(4,8);
+mul(7,8);
+
+console.log("End Here");
